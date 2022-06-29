f3aa0dc56d701e44c23739acb1c827127f4efa41
TusharDahule <tushardahule@gmail.com>
2022-06-29 18:18:47 +0530

    pass by value / reference added

--- index.js
+++ index.js
@@ -78,6 +78,8 @@ console.log(addArrayElementsWithoutUsingGlobalVariable([1, 2, 3, 4, 5]));
 
 console.log('----------miscellaneous problems in js---------------');
 
+/* difference between arrow function and traditional function */
+
 const obj1 = {
   addMe: function () {
     return this;
@@ -93,3 +95,88 @@ const obj2 = {
 };
 
 console.log(obj2.addMe()); /* refers to global window object */
+
+/* miscellaneous problem */
+var a = 1;
+console.log('a: ', a);
+
+addition();
+console.log('after addition, a: ', a);
+
+var a = 3;
+console.log('a: ', a);
+
+function addition() {
+  a = a + a;
+}
+
+addition();
+console.log('After addition, a: ', a);
+
+xsx = a;
+console.log('xsx: ', xsx);
+
+function assignValueToUndeclaredVar() {
+  xsx2 = a;
+  console.log('from function scope, xsx2 :', xsx2);
+  {
+    xsx3 = a;
+    console.log('from block scope, xsx3 :', xsx3);
+  }
+}
+
+assignValueToUndeclaredVar();
+console.log('from global scope, xsx2 : ', xsx2);
+console.log('from global scope, xsx3 :', xsx3);
+
+/* pass by value */
+console.log('-----------pass by value-------------');
+
+var b = 10;
+var d = b;
+console.log('d :', d);
+
+function increment(num) {
+  num = num + 1;
+}
+
+increment(d);
+
+console.log('after increment, d :', d);
+
+/* pass by reference */
+console.log('-----------pass by reference-------------');
+
+var employeeObj = {
+  id: 100,
+  name: 'Tushar',
+  salary: 50000,
+};
+
+console.log('employeeObj :', employeeObj);
+
+function doubleSalary(employeeObj) {
+  employeeObj.salary = employeeObj.salary * 2;
+}
+
+doubleSalary(employeeObj);
+
+console.log('after doubling salary, employeeObj :', employeeObj);
+
+function changeObjectReference(employeeObj) {
+  employeeObj = {
+    id: 101,
+    name: 'Ajay',
+    salary: 30000,
+  }; /* reference cannot be changed. This will create a new employeeObj referencing to Ajay employee */
+  console.log(
+    'after changing reference, employeeObj from inside the method:',
+    employeeObj
+  );
+}
+
+changeObjectReference(employeeObj);
+console.log(
+  'after changing reference, employeeObj :',
+  employeeObj
+); /* Still prints Employee Tushar object */
